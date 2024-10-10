844c54f043fc7f10fd56671e1c92f4055a9ad34e
NjugunaKelvin <njugunak349@gmail.com>
2024-10-10 14:03:28 +0300

    initial

---
+++ index.py
@@ -0,0 +1,7 @@
+weight = float(input('Enter your weight in kgs: '))
+height = float(input('Enter your height in metres \n'))
+
+# BMI = weight/height**2
+BMI = int(weight/(height**2))
+
+print(f"Your BMI is {BMI}")
